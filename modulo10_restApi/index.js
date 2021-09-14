05ffc4259f3e5510ed5bc34d1ac5593462b27750
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-09-14 16:38:08 -0300

    Configurando o banco de dados

--- modulo10_restApi/index.js
+++ modulo10_restApi/index.js
@@ -8,6 +8,16 @@ const app = express()
 //Definição de porta
 const port = 3000
 
+// Abrindo conexao com base de dados
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Linux@31',
+  database: 'sistema_noticias'
+});
+
+connection.connect()
+
 // Serviço de Hello Word
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -15,9 +25,25 @@ app.get('/', (req, res) => {
 
 // Serviço de busca de categorias
 app.get('/news-api/v1/categorias', (req, res) => {
-    let categorias = []
-    res.send(categorias)
+
+    // Aqui faz a busca por categorias
+    connection.query('SELECT id, nome FROM sistema_noticias.categoria', function(err, rows, fields) {
+      if (err) throw err;
+        res.send(rows)
+    });    
   })
+
+// Serviço de busca de noticias
+app.get('/news-api/v1/categorias/:categoriaId/noticias', (req, res) => {
+
+  res.send(req.params.categoriaId)
+
+  // Aqui faz a busca por categorias
+  // connection.query('SELECT id, nome FROM sistema_noticias.categoria', function(err, rows, fields) {
+  //   if (err) throw err;
+  //     res.send(rows)
+  // });    
+})
 
 //subindo servidor Node
 app.listen(port, () => {
