9d888568d97b3fabe794fa4d4e6522ed8cb2c63b
Ricardo Correa Ribeiro <ricardocribeiro@hotmail.com>
2021-10-07 19:34:27 -0300

    configurando o slide da página principal

--- desafiochefao/src/components/SobreNos/SobreNos.js
+++ desafiochefao/src/components/SobreNos/SobreNos.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react';
-import { Carousel } from 'react-bootstrap';
+import Slider from "react-slick";
+import "./estilo.css";
 
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 class SobreNos extends Component {
-    render(){
-        return(
+    render() {
+        const settings = {
+        
+         
+          infinite: true,
+          slidesToScroll: 2,
+          slidesToShow: 2,
+          speed: 500
+        };
 
-            <p> aqui vai estar o carrossel</p>
-
-        )
-
+        return (
+            
+          <div className= 'SobreNos'>
+            <h2> O que dizem sobre nós</h2>
+            <Slider {...settings}>
+              <div>
+                <h4>Mentorando 10</h4>
+                <h3>Cameron</h3>
+                <h5>Estudante</h5>
+              </div>
+              <div>
+              <h4>Mentorando 20</h4>
+                <h3>Priscila</h3>
+                <h5>Mentora</h5>
+              </div>
+              <div>
+              <h4>Mentorando 30</h4>
+                <h3>Bruno</h3>
+                <h5>Empresa</h5>
+              </div>
+              <div>
+              <h4>Mentorando 40</h4>
+                <h3>Bruno</h3>
+                <h5>Empresa</h5>
+              </div>
+              
+            </Slider>
+          </div>
+        );
     }
+                  
 }
 
 export default SobreNos;
